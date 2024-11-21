798695dfbe86d1944e9049fcacdf9f04e38ce0ea
Leandro Schwegler <leandro_schwegler@sluz.ch>
2024-11-21 15:42:30 +0100

    ex1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
+	firstName := "Leandro"
+	lastName := "Schwegler"
+	dayOfBirth := 12
+	monthOfBirth := 1
+	yearOfBirth := 2008
+	numberOfSiblings := 1
+	heightInMeters := 1.80
+	zodiacSign := 'S'
+
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
