5fd5e1667139293fbc6e63decb7ba93aee9d3800
Leandro Schwegler <leandro_schwegler@sluz.ch>
2024-12-05 15:58:28 +0100

    go-3-ex-1,2,3,4

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -22,6 +22,33 @@ func outputWithZodiacSign(p Person) {
 
 	// TODO: Assign proper value to zodiacSign using if/else branching.
 	// NOTE: The runes are defined above as constants.
+	day, month := p.Day, p.Month
+
+	if (month == 3 && day >= 21) || (month == 4 && day <= 19) {
+		zodiacSign = Aries
+	} else if (month == 4 && day >= 20) || (month == 5 && day <= 20) {
+		zodiacSign = Taurus
+	} else if (month == 5 && day >= 21) || (month == 6 && day <= 20) {
+		zodiacSign = Gemini
+	} else if (month == 6 && day >= 21) || (month == 7 && day <= 22) {
+		zodiacSign = Cancer
+	} else if (month == 7 && day >= 23) || (month == 8 && day <= 22) {
+		zodiacSign = Leo
+	} else if (month == 8 && day >= 23) || (month == 9 && day <= 22) {
+		zodiacSign = Virgo
+	} else if (month == 9 && day >= 23) || (month == 10 && day <= 22) {
+		zodiacSign = Libra
+	} else if (month == 10 && day >= 23) || (month == 11 && day <= 21) {
+		zodiacSign = Scorpius
+	} else if (month == 11 && day >= 22) || (month == 12 && day <= 21) {
+		zodiacSign = Sagittarius
+	} else if (month == 12 && day >= 22) || (month == 1 && day <= 19) {
+		zodiacSign = Capricornus
+	} else if (month == 1 && day >= 20) || (month == 2 && day <= 18) {
+		zodiacSign = Aquarius
+	} else if (month == 2 && day >= 19) || (month == 3 && day <= 20) {
+		zodiacSign = Pisces
+	}
 
 	fmt.Printf("%s %s, born on %02d.%02d.%04d, has the zodiac sign %c.\n",
 		p.FirstName, p.LastName, p.Day, p.Month, p.Year, zodiacSign)
